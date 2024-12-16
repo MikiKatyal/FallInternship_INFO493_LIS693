08c64b86f3a3f45ce3cd0bf1109fd78541e146ee
MikiKatyal <156737485+MikiKatyal@users.noreply.github.com>
2024-12-15 19:09:46 -0700

    Add files via upload

---
+++ Project - Dictionary_C#/02_Srouce_Code/00_SampleCode/clsTemplate.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CF_CHash_Dictionary._00_SampleCode
+//{
+//    internal class clsTemplate
+//    {
+//    }
+//}
+
+//        #region ------Function Structure
+
+//        //public bool FunctionName (string Parameter_1,ref int Parameter_2, string Parameter = clsGlobalEnum.CgstrBlank, out String Parameter_4)
+//        //    {
+//        //        //----/* Declaration - Private Object variables */----
+//        //        //----/* Declaration - Private variables */----
+//        //        bool ExecutionComplete;
+//        //        bool ReturnValue;
+//        //        string ThisFunctionName = System.Reflection.MethodInfo.GetCurrentMethod().Name;
+
+//        //        //----/* Initialize - Variables */----
+//        //        //--Local Variables
+//        //        ExecutionComplete = false;
+//        //        ReturnValue = false;
+
+//        //        //--Parameters
+
+//        //        try
+//        //        {
+//        //        //----/* Functionality  - Start */----
+
+//        //        ExecutionComplete:
+//        //          ExecutionComplete = true;
+//        //        //----/* Functionality  - End */----
+
+//        //        ExitFunction:;
+//        //        }
+
+//        //        catch (Exception objException)
+//        //        {
+//        //            clsGlobalErrorHandler.GenericErrorHandler(objException, ThisClassName, ThisFunctionName);
+//        //        }
+//        //        finally
+//        //        {
+//        //        //----/* Close - Variables */----
+//        //        if (ExecutionComplete == true) ReturnValue = true;
+
+//        //        }
+
+//        //        //----/* Return Execution Status */----
+//        //        return ReturnValue;
+//        //    }
+//        #endregion
